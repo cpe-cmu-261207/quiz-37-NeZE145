3e2eba97546fff2dc8d0c6112687249ca44b4e3d
NeZE <boss-NeZE@hotmail.com>
2022-09-15 14:47:53 +0700

    asd

--- components/Comment.jsx
+++ components/Comment.jsx
@@ -1,13 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import { comments } from "../libs/comments";
 import Reply from "./Reply";
 
 export default function Comment(props) {
+  const [text] = useState("");
+  const textfilter = comments.filter((x) => x.username.includes(text));
+
   return (
     <div>
       {/* Entire Comment div */}
-      <div></div>
-
-      {/*render Reply here... */}
+      {textfilter.map((x) => (
+        <div className="d-flex gap-2 my-2">
+          <img
+            src={x.userImagePath}
+            width="48"
+            height="48"
+            className="rounded-circle"
+            style={{ objectFit: "cover" }}
+          />
+          <div
+            className="rounded rounded-3 p-2"
+            style={{ backgroundColor: "#E5E7EB" }}
+          >
+            <span className="fw-semibold">{x.username}</span>
+            <br />
+            <span>{x.commentText}</span>
+            <div className="d-flex align-items-center gap-1">
+              <img src="/like.svg" width={20}></img>
+              <span className="text-muted">{x.likeNum} คน</span>
+            </div>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
--- components/PostOwner.jsx
+++ components/PostOwner.jsx
@@ -1,5 +1,26 @@
 import React from "react";
 
 export default function PostOwner(props) {
-  return <div></div>;
+  return (
+    <div className="vstack gap-3">
+      <div className="d-flex align-items-center gap-3">
+        <img
+          src="/profileImages/morehandsome.jpeg"
+          width="48"
+          height="48"
+          className="rounded-circle"
+          style={{ objectFit: "cover" }}
+        />
+        <span className="fw-semibold fs-5">
+          Wasan Santichaiphonkul 640610667
+        </span>
+      </div>
+      <span>Quiz ยากแท้ #261207</span>
+      <div className="d-flex align-items-center gap-1">
+        <img src="/like.svg" width={20}></img>
+        <span className="text-muted">100 คน</span>
+      </div>
+      <hr className="m-0" />
+    </div>
+  );
 }
